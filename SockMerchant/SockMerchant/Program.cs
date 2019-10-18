213e7858e0f6cb0568e1baa1ada8adb432db1a16
Richard <richardwjmorrow@gmail.com>
2019-10-18 08:35:13 -0700

    SockMerchant exercise

---
+++ SockMerchant/SockMerchant/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar)
+    {
+        Array.Sort(ar);
+        int pairs = 0;
+        for ( int i = 0; i < n-1; i++)
+        {
+            if (ar[i]== ar[i + 1])
+            {
+                i += 1;
+                pairs++;
+            }
+        }
+
+        return pairs;
+    }
+
+    static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = 9; // Convert.ToInt32(Console.ReadLine());
+
+        int[] ar =  new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20} //Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = sockMerchant(n, ar);
+        Console.WriteLine(result);
+        //textWriter.WriteLine(result);
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}

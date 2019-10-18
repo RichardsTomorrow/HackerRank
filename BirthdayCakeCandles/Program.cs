06dbc44424e853f400e023770394a2e94c2f5b1b
Richard <richardwjmorrow@gmail.com>
2019-10-18 08:43:59 -0700

    birthday cake candles

---
+++ BirthdayCakeCandles/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the birthdayCakeCandles function below.
+    static int birthdayCakeCandles(int[] ar)
+    {
+        int age = ar.Length;
+        Array.Sort(ar);
+        int sum = 0;
+        for (int i =0; i < age; i++)
+        {
+            if (ar[age - 1] == ar[i])
+            {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = birthdayCakeCandles(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}

98c5f3ef74522edb23c996425cfbe456371382c5
Richard <richardwjmorrow@gmail.com>
2019-10-20 14:01:12 -0700

    finished exercise!

---
+++ IntroToConditionals/IntroToConditionals/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+
+
+    static void Main(string[] args)
+    {
+        int N = 22; //Convert.ToInt32(Console.ReadLine());
+
+        if ( N % 2 != 0)
+        {
+            Console.WriteLine("Weird");
+        }
+        else if (N == 2 || N == 4)
+        {
+            Console.WriteLine("Not Weird");
+        }
+        else if (N >= 6 && N <= 20)
+        {
+            Console.WriteLine("Weird");
+        }
+        else
+        {
+            Console.WriteLine("Not Weird");
+        }
+    }
+}

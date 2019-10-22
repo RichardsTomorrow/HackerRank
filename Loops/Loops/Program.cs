fda1b7e5bd99e873a19c56fe18c6a42f5329d3ca
Richard <richardwjmorrow@gmail.com>
2019-10-22 10:19:42 -0700

    finished 30 days of code: loops

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        for (int i = 0; i < 10; i++)
+        {
+            int result = n * (i + 1);
+            Console.WriteLine($"{n} x {i+1} = {result}");
+        }
+    }
+}

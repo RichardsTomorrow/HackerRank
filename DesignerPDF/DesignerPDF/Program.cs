cfe8c54ede14ef4fb09510a6912ac813efc001d8
Richard <richardwjmorrow@gmail.com>
2019-10-22 19:23:48 -0700

    completed the designer PDF exercise

---
+++ DesignerPDF/DesignerPDF/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the designerPdfViewer function below.
+    static int designerPdfViewer(int[] h, string word)
+    {
+        char[] alpha = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        int maxHeight = 0;
+        int temp = 0;
+
+        for(int i =0; i < h.Length; i++)
+        {
+            foreach(char letter in word)
+            {
+                if (alpha[i] == letter)
+                {
+                    temp = h[i];
+                }
+                if(temp > maxHeight)
+                {
+                    maxHeight = temp;
+                }
+            }
+            
+        }
+
+        return maxHeight * word.Length;
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int[] h = Array.ConvertAll(Console.ReadLine().Split(' '), hTemp => Convert.ToInt32(hTemp))
+        ;
+        string word = Console.ReadLine();
+
+        int result = designerPdfViewer(h, word);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+

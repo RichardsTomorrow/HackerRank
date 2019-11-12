2c90e5461b946f914d298086b50fd3d6561fcb21
Richard <richardwjmorrow@gmail.com>
2019-11-12 13:33:39 -0800

    moved things

---
+++ csharp/JumpingOnTheCloudsRevisted/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c, int k)
+    {
+        int jumps = 0;
+        int i = 0;
+        int e = 100; //starting energy is at 100
+        while (i < c.Length - 1)
+        {
+            if (i + 2 < c.Length && c[i + 2] == 0)
+            {
+                i += 2;
+                jumps++;
+            }
+            else
+            {
+                i++;
+                jumps++;
+            }
+
+        }
+        return jumps;
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nk = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nk[0]);
+
+        int k = Convert.ToInt32(nk[1]);
+
+        int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp))
+        ;
+        int result = jumpingOnClouds(c, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}

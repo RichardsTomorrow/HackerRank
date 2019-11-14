df3a9f19a7fa3a7518507cbd21f5b1e3caf09034
Richard <richardwjmorrow@gmail.com>
2019-11-14 13:04:43 -0800

    slightly better solution

--- javascript/JavaRegEx/RegEx1.js
+++ javascript/JavaRegEx/RegEx1.js
@@ -27,7 +27,7 @@ function regexVar() {
      * Declare a RegExp object variable named 're'
      * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
      */
-    const re = /^(a|e|i|o|u).*\1$/ig
+    const re = /^([aeiou]).*\1$/ig
     
     /*
      * Do not remove the return statement

cf0afaabc03606f3f176ba32ebe0513c027da265
Richard <richardwjmorrow@gmail.com>
2019-11-14 15:23:33 -0800

    finished CreateButton

---
+++ javascript/CreateButton/js/button.js
@@ -0,0 +1,9 @@
+var clickMeButton = document.createElement('button');
+let num = 0;
+clickMeButton.innerHTML = num;
+clickMeButton.id = 'btn';
+document.body.appendChild(clickMeButton);
+clickMeButton.onclick = function() {
+    num++;  
+    clickMeButton.innerHTML = num;
+};
